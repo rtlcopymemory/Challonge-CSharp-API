5f75ab58af9f219f7b1e3ad54b560cc81629bda8
StackWolfed <stackwolfed@protonmail.com>
2019-10-27 16:55:42 +0100

    Adds the Match Attachments Endpoint

---
+++ MatchAttachments.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challonge_API {
+    class MatchAttachments {
+        static async Task<JObject> Index(Api api, string tournament, string matchId, Dictionary<string, string> parameters) {
+            string path = "tournaments/" + tournament + "/matches/" + matchId + "/attachments.json";
+
+            return await api.FetchAndParse(Api.Methods.GET, path, parameters);
+        }
+
+        static async Task<JObject> Create(Api api, string tournament, string matchId, Dictionary<string, string> parameters) {
+            string path = "tournaments/" + tournament + "/matches/" + matchId + "/attachments.json";
+
+            return await api.FetchAndParse(Api.Methods.POST, path, parameters);
+        }
+
+        static async Task<JObject> getAttachment(Api api, string tournament, string matchId, string attachmentId, Dictionary<string, string> parameters) {
+            string path = "tournaments/" + tournament + "/matches/" + matchId + "/attachments/" + attachmentId + ".json";
+
+            return await api.FetchAndParse(Api.Methods.GET, path, parameters);
+        }
+
+        static async Task<JObject> UpdateAttachment(Api api, string tournament, string matchId, string attachmentId, Dictionary<string, string> parameters) {
+            string path = "tournaments/" + tournament + "/matches/" + matchId + "/attachments/" + attachmentId + ".json";
+
+            return await api.FetchAndParse(Api.Methods.PUT, path, parameters);
+        }
+
+        static async Task<JObject> DeleteAttachment(Api api, string tournament, string matchId, string attachmentId, Dictionary<string, string> parameters) {
+            string path = "tournaments/" + tournament + "/matches/" + matchId + "/attachments/" + attachmentId + ".json";
+
+            return await api.FetchAndParse(Api.Methods.DELETE, path, parameters);
+        }
+    }
+}
